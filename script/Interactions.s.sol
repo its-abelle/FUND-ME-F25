0b60d989b93ddc9d556477c94451af0b38fea437
its-abelle <aquinarian@gmail.com>
2025-10-06 11:23:35 +0300

    Fixing the file path of DevOpsTools in the Interactions.sol file

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract FundFundMe is Script {
     uint256 constant SEND_VALUE = 0.1 ether;
